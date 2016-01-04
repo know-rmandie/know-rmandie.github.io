a44006eeb06d47aa8445f10f1e6fcf4769874a32
Sylvain Comte <sylvain.comte@developpement-durable.gouv.fr>
2016-01-04 17:44:48 +0100

    first init of gh-pages version

---
+++ README.md
@@ -0,0 +1,28 @@
+﻿*Le [projet original](https://git.framasoft.org/territoires/homeCommon) est hébergé sur [l'instance gitlab de Framasoft](https://git.framasoft.org). D'autres versions peuvent être déployées, notamment sur github, mais il s'agit de mirroirs...*
+
+# Outils et pages au service des territoires
+Ce répertoire contient plusieurs projets destinés à capitaliser et mettre en forme des informations sur les territoires. C'est un travail initié pour la Normandie, mais adaptable à n'importe quel autre territoires... 
+
+## Projets diponibles
+*nb : les liens sont conçus pour fonctionner sur git.framasoft.org . Si vous être sur une copie hébergée ailleurs, il se peut qu'ils ne fonctionnent pas*
+* [analyse des documents d'urbanisme](../../../analyseUrbanisme/) : une analyse de l'évolution des documents d'urbanisme à partir des données de sudocu. Notamment des diagrammes de Sankey, et des commentaires associés,
+* [cartographie de la planification](../../../cartoPlanification) : une cartographie des Schémas de cohérence territoriale et de leur avancement,
+* [cartographie de la ville durable](../../../cartoVilleDurable) : Une cartographie des projets d'écoquartiers, avec des éléments sur leur avancement et leur labellisation,
+* Pas encore partagés sur ce répertoire :
+  * Indicateurs économiques pour le suivi des territoires,
+  * Indicateurs de consommation d'espace.
+
+## Adaptation / utilisation
+L'ensemble est conçu pour fonctionner comme des pages web simple.
+Structure de base pour respecter les dépendances existantes :
+* repertoire d'installation
+  * planification : contient [cartoPlanification](../../../cartoPlanification)
+  * urbanisme : contient [analyseUrbanisme](../../../analyseUrbanisme)
+  * ville-durable : contient [cartoVilleDurable](../../../cartoVilleDurable)
+
+Tout ceci est modifiable, il faut juste `index.html` et l'arborescence en fonction des modules qu'on souhaite mettre en place.
+
+## Sources utilisées / licences
+Chaque module présente les sources et codes qu'il utilise ainsi que les licences associées. La page d'accueil utilise seulement [jQuery](https://jquery.org) sous Licence MIT.
+  
+Sauf mention contraire les codes sont **réutilisable sous licence [MIT](http://opensource.org/licenses/MIT)** et les **icones, textes et objets graphiques sous licence [CC By 3.0](http://creativecommons.org/licenses/by/3.0/deed.fr)**.
---
+++ js/init-hr.js
@@ -0,0 +1,107 @@
+﻿/* global functions and variables */
+var $window,$body;		// portent bien leur nom
+var getWidth;			// fonction de mise à la bonne largeur (en fonction de l'usage)
+var rW=10,rH=5,rW0,rWg,rWi; // largeur et hauteur de la zone de tracé + variables largeurs initiales graphes et illustrations
+
+(function($) {
+	$(function() {
+		$window = $(window),
+		$body = $('body');
+	// Disable animations/transitions until the page has loaded.
+		$body.addClass('is-loading');
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+		});
+		
+	// removable menu for handeld device
+		$('#navIcon').click(function(){
+			$('#nav').toggleClass('active')
+			});
+		
+	// function that get width of #main element
+	getWidth=function(usage) {
+		switch(usage) {
+			case 'map' : 
+				rW=$('#main').width()-10;	
+				rH=$(window).height()-240;
+				break;
+			case 'graph' :
+				rW=$('#main').width();
+				rH=rW/2;
+				break;
+			case 'full' : 
+				rW=$('#main').width();	
+				rH=$(window).height();
+				break;
+			/*case 'illus' :
+				rW=$('#main').width();
+				if(rW>730) rW=rW/2;
+				rH=rW/2;
+				if(rWi==null) rWi=rW;
+				break;*/
+			default :
+				rW=$('#main').width();
+				rH=$(window).height();
+				if(rW0==null) rW0=rW;
+			}
+	}	
+		
+/* Comments and bug report */
+	// require data in ../user/token.js
+	// show and hide comments form
+	function letsSendComment() {
+	   $('#comment-fond').fadeIn('.5s');
+	   $('#commentaires').fadeIn('.75s')
+	   }
+	function letsCloseComment() {
+	   $('#commentaires').fadeOut('.5s');
+	   $('#comment-fond').fadeOut('.75s');
+	   $('#form').show('.77s');
+	   $('#thanks').hide('.77s');
+	   $('input, textarea').val(function() {
+			return($(this).attr('default'));
+			});
+		$('textarea').text($(this).attr('default'));
+	   }
+	   
+	// add functions to links and buttons
+	$('#letsComment').click(letsSendComment);
+	$('#comment-close').click(letsCloseComment);
+	// add function to submit button
+	$('#submit').click(FormSubmit);
+	
+	// submission function
+	function FormSubmit() {
+		// define target url
+		var formUrl= 'https://'+gitlab+'/api/v3/projects/'+project+'/issues?private_token='+token;
+		// set some attributes to form
+		var $form=$('#form');
+		$form.attr('target','sender');
+		$form.attr('action',formUrl);
+		$form.attr('method','POST');
+		// get form data before posting
+		var formData = $form.serialize();
+		console.log('FORM DATA:\n' + formData);
+		// post data
+		$form.submit(function(evt) {
+			var titCom=$('#commentaires input[type=text]');
+			var comCom=$('#commentaires textarea');
+			console.log(titCom.val()+" - "+comCom.text());
+			console.log(titCom.attr('default')+" - "+comCom.attr('default'));
+			if(titCom.val()===titCom.attr('default')) {titCom.val('');evt.preventDefault();}
+			else {
+				if(comCom.text()===comCom.attr('default')) {
+					comCom.text('');evt.preventDefault();}
+				else {
+					// hide form, display thanks
+					$('#form').fadeOut();
+					$('#thanks').fadeIn('slow');
+					// close comments
+					window.setTimeout(letsCloseComment,3000);
+					// clean form : reset to default value
+					}
+				}
+			});
+		}	
+	})
+})(jQuery);
