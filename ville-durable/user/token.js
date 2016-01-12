5beacf9b172729dc6821a41d549abd9f28294546
Sylvain Comte <sylvain.comte@developpement-durable.gouv.fr>
2016-01-12 15:24:10 +0100

    updated token to enable issue posting

--- ville-durable/user/token.js
+++ ville-durable/user/token.js
@@ -2,7 +2,7 @@
 /*---------------*/
 /*
 this is a pretty INSECURE way to make issues sent to your gitlab project.
-Giving you own token with full privilege here CAN NOT BE SECURE. It can be grabbed by any (even very bad) hacker and give him access to your whole projects on gitlab.
+Giving your own token with full privilege here CAN NOT BE SECURE. It can be grabbed by any (even very bad) hacker and give him access to your whole projects on gitlab.
 So you SHOULD create a secondary account with only GUEST PRIVILEGE and grab corresponding token here. So the hacker will "only" spam your issue box until you reset token.
 */
 // gitlab repo url
@@ -10,4 +10,4 @@ var gitlab="git.framasoft.org";
 // project id in gitlab
 var project="territoires%2FcartoVilleDurable";
 // token !WARNING!
-var token="this_is_your_SECONDARY_gitlab_token_with_ONLY_GUEST_PRIVILEGE";
+var token="84AixuCbHNbTx36_gmUw";
