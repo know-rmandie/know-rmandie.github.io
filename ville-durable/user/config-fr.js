d18bf8377fd5bc93fb9b1c1902dbd3f903dc14a5
Sylvain Comte <sylvain.comte@developpement-durable.gouv.fr>
2016-01-12 15:46:50 +0100

    ajout des fonds osm noir et blanc et utilisation pour la carte écoquartiers

--- ville-durable/user/config-fr.js
+++ ville-durable/user/config-fr.js
@@ -2,5 +2,5 @@
 var knwrmdLat=47.1;	// latitude du centre de la carte au premier chargement
 var knwrmdLong=2.4;		// longitude du centre de la carte au premier chargement
 var knwrmdZoom=5;	// niveau de zoom entre 1 et 20 au premier chargement
-var knwrmdFond="water";	// identifiant du fond chargé (cf. ../data/fonds-hr.json)
+var knwrmdFond="osmbw";	// identifiant du fond chargé (cf. ../data/fonds-hr.json)
 var clusterRadius=75;	// taille de la zone de clusterisation
--- ville-durable/user/config.js
+++ ville-durable/user/config.js
@@ -2,5 +2,5 @@
 var knwrmdLat=49.4;	// latitude du centre de la carte au premier chargement
 var knwrmdLong=0;		// longitude du centre de la carte au premier chargement
 var knwrmdZoom=null;	// niveau de zoom entre 1 et 20 au premier chargement
-var knwrmdFond="water";	// identifiant du fond chargé (cf. ../data/fonds-hr.json)
+var knwrmdFond="osmbw";	// identifiant du fond chargé (cf. ../data/fonds-hr.json)
 var clusterRadius=80;	// taille de la zone de clusterisation
