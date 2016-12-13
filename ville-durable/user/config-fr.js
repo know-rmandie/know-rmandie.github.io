3a923e6f001269a834083e74a34ea12b150dfe1e
Sylvain Comte <sylvain.comte@yahoo.fr>
2016-12-13 18:21:45 +0100

    mise à jour de la carte de France ville-durable : données fin 2016 + différence entre "labellisé" et "tous projets" passage à leaflet 1 corrections de scripts diverses associées

--- ville-durable/user/config-fr.js
+++ ville-durable/user/config-fr.js
@@ -3,4 +3,4 @@ var knwrmdLat=47.1;	// latitude du centre de la carte au premier chargement
 var knwrmdLong=2.4;		// longitude du centre de la carte au premier chargement
 var knwrmdZoom=5;	// niveau de zoom entre 1 et 20 au premier chargement
 var knwrmdFond="osmbw";	// identifiant du fond chargé (cf. ../data/fonds-hr.json)
-var clusterRadius=75;	// taille de la zone de clusterisation
+var clusterRadius=35;	// taille de la zone de clusterisation
