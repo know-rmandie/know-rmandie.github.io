71eccadcdbec88205d71e8b2b609c3c72f0a2efc
Sylvain Comte <sylvain.comte@developpement-durable.gouv.fr>
2017-06-15 09:33:48 +0200

    les échelons géographiques semblent fonctionner
    reste à régler le cas des données manquantes

--- I/CE/js/init.js
+++ I/CE/js/init.js
@@ -2,15 +2,17 @@
     $(function() {
         /* --- */
 // outils de débugage
-var tst = 0, tst2 = 0;
-function tester(v) {
-    if(tst < 10) {
+var Tst = [];
+function tester(v,text) {
+    if(Tst[text] < 10 || Tst[text] === undefined) {
+        if(Tst[text] === undefined) Tst[text] = 0;
+        console.log("test > "+text);
         console.log(v);
-        tst++;
+        Tst[text]++;
     }
 }
-
-var firstTime = 1;
+// variables globales
+var Geo = [], firstTime = 1;
 // -----------------
         // masquage du pilote
         $("#pilote").fadeOut();
@@ -42,12 +44,11 @@ var firstTime = 1;
             // remplace le message d'attente par le pilote de recherche
             $("#wait").fadeOut();
             $("#pilote").fadeIn();
-            // préparation des données
+            // préparation des données de niveaux territoriaux
             var territ = assoArray(res[0],"id"),
                 oscom = res[1],
                 oscleg = res[2],
                 etb = res[3];
-            console.log(territ);
             for(var i in territ) {
                 territ[i].label = territ[i].Nom;
                 territ[i].value = territ[i].id;
@@ -114,12 +115,14 @@ var firstTime = 1;
                         break;
                     }
                 }
+                // récupération des niveaux géographiques
+                Geo = geoLevels(id,territ);
+                // lancement des différents traitements
                 drawOs(id);
                 writeIC(id);
             }
             // tracé du graphe d'occupation des sols
             function drawOs(id) {
-                tester(id);
                 $("#titreZone").text(territ[id].Nom);
                 var svg = d3.select("svg");
                     svg.attr("width",document.getElementById("OcSol").clientWidth);
@@ -142,59 +145,21 @@ var firstTime = 1;
                     .offset(d3.stackOffsetExpand);
 
                 var data = [];
+                // récupération des en-têtes de colonnes
                 data.columns =  oscom.columns;
-                // récupération de l'identifiant de la commune centre
-                var com = territ[id].c;
-                // si on a un "centre", on "recentre"
-                var Geo = [], ord = 0, sub = null;
-                if(com !== "" && com !== null && com !== undefined) {
-                    // si notre territoire est une commune et comporte un "centre", elle a été fusionnée commune nouvelle
-                    if(territ[id].type === "c") {
-                        sub = id.substr(1,id.length);
-                        Geo["sub"]={"id":sub,"order":ord};
-                        ord++;
-                    }
-tester(sub);
-                    // sinon, c'est un epci, un SCoT ou autre
-                    // dans tous les cas on repart de la commune nouvelle
-                    id = "i"+com;
-                }
-                else com = id.substr(1,id.length);
-                Geo["com"]={"id":com,"order":ord};ord++;
-                if(territ[id].e !== undefined) {
-                    Geo["epci"]={"id":territ[id].e,"order":ord};
-                    ord++};
-                /*if(territ[id].s !== "" ) {
-                    Geo.push({"scot":{"id":territ[id].s,"order":ord}});
-                    ord++};*/
-                Geo["dep"]={"id":com.substr(0,2),"order":ord};ord++;
-                Geo["reg"]={"id":"Norm","order":ord};
-                /* remplissage du tableau de données en partant du "centre";
-                if(sub !== "" && sub !== null && sub !== undefined) {Ord["sub"] = order; order++};
-                Ord["com"] = order; order++;
-                if(epci !== "" && epci !== null && epci !== undefined) {Ord["epci"] = order; order++};
-                if(scot !== "" && scot !== null && scot !== undefined) {Ord["scot"] = order; order++};
-                Ord["dep"] = order; order++;
-                Ord["reg"] = order; order++;*/
-tester(Geo);
+                // mise en place des données dans la table
                 for (var i in oscom) {
                     for (var geo in Geo) {
                         if (oscom[i].insee_2015 === Geo[geo].id) data[+Geo[geo].order] = oscom[i];
                     }
                 }
-                // valeurs non fournies par oscom dans data :
-                for (var i=0; i<ord;i++) {
-                    if(data[i] === undefined) data[i]=0;
+                // valeurs non fournies par oscom dans data : mise à 0
+                // !!todo pour les niveaux supérieurs aux communes, on peut reconstituer avec un sumif
+                for (var i=0; i<data.length; i++) {
+                    console.log(i);
+                    if(data[i] === undefined) data.splice(i,1);
                 }
-
-                    /*
-                    if (oscom[i].insee_2015 === com) data[Ord["com"]] = oscom[i];
-                    // !!todo récupération des entités supérieures
-                    if(Ord["epci"] !== undefined) {if (oscom[i].insee_2015 === epci) data[Ord["epci"]] = oscom[i];}
-                    if(Ord["scot"]) {if (oscom[i].insee_2015 === scot) data[Ord["scot"]] = oscom[i];}
-                    if (oscom[i].insee_2015 === dep) data[Ord["dep"]] = oscom[i];
-                    if (oscom[i].insee_2015 === "Norm") data[Ord["reg"]] = oscom[i];*/
-tester(data);
+                // construction du graphique si on a des données
                 if (data.length > 0) {
                     // construction de l'axe y
                     yOs.domain(data.map(function(d) {
@@ -265,37 +230,73 @@ tester(data);
 
             // tableau des données ETB
             function writeIC(id) {
-                // récupération de l'identifiant du département
-                var dep = id.substr(0,2);
+                // récupération des niveaux géographiques
                 // nettoyage de la table existante
                 $('#iCons table tr').remove();
                 // mise en place de la table
                 var table = d3.select('#iCons').append('table');
+                // en-têtes
                 var thead = table.append('tr');
                     thead.append('th').text('Nom');
-                    thead.append('th').text('Surface utilisée');
                     thead.append('th').text('Locaux construits');
-                    thead.append('th').text('Densité moyenne');
-                var tr0 = table.append('tr');
-                var tr4 = table.append('tr');
-                var tr5 = table.append('tr');
-                function createLine(l,x) {
-                    l.append('td').text(territ["i"+etb[x].insee_2017].Nom);
-                    l.append('td').attr('class','real').text(Math.round(etb[x].cons/100)/100);
-                    l.append('td').attr('class','int').text(etb[x].loc);
-                    l.append('td').attr('class','real').text(Math.round(etb[x].dens*100)/100);
+                    thead.append('th').text('Surface utilisée');
+                    thead.append('th').text('Densité');
+                    thead.append('th').text('Intensité');
+
+                // création des lignes pour chaque niveau géographiques
+                var Line = [];
+                for(var geo in Geo) {
+                    Line[Geo[geo].order] = table.append('tr');
                 }
+                // remplissage des lignes
                 for (var t in etb) {
-                    if(etb[t].insee_2017 === id) createLine(tr0,t);
+                    for (var geo in Geo) {
+                        if(etb[t].insee_2017 === Geo[geo].id) {
+                            var l = Line[Geo[geo].order];
+                            l.append('td').text(territ['i'+etb[t].insee_2017].Nom);
+                            l.append('td').attr('class','int').text(etb[t].loc);
+                            l.append('td').attr('class','real').text(Math.round(etb[t].cons/100)/100);
+                            l.append('td').attr('class','real').text(Math.round(etb[t].dens*100)/100);
+                            l.append('td').attr('class','real').text(Math.round(etb[t].loc/territ["i"+etb[t].insee_2017].pop*1000/10*100)/100);
+
+                        }//fillLine(Line[Geo[geo].order],t);
+                    }
+                    /*if(etb[t].insee_2017 === id) createLine(tr0,t);
                     if(etb[t].insee_2017 === dep) createLine(tr4,t);
-                    if(etb[t].insee_2017 === "Norm") createLine(tr5,t);
+                    if(etb[t].insee_2017 === "Norm") createLine(tr5,t);*/
                 }
+                // ajout des sources
                 var iConsSource = d3.select('#iConsLeg').append("p").attr("class","source")
                     .html("source : <a href='http://www.epf-normandie.fr/Actualites/A-la-Une/Accompagnement-de-l-EPF-Normandie-dans-la-mesure-de-la-consommation-fonciere-a-l-echelle-regionale-Mise-en-ligne-de-la-base-de-donnees-Extension-du-Tissu-Bati-ETB'>Extension du Tissu Bâti</a> (ETB) 2004 > 2013 - <a href='http://www.epf-normandie.fr/'>EPF Normandie</a> - 2016");
             }
          }
 
-
+         // fonction de construction des niveaux géographiques
+         function geoLevels(id,base) {
+             // récupération de l'identifiant de la commune centre (si existe)
+             var com = base[id].c;
+             // si on a un "centre", on "recentre"
+             var Levels = [], ord = 0, sub = null;
+             if(com !== "" && com !== null && com !== undefined) {
+                 // si notre territoire est une commune, elle a été fusionnée commune nouvelle
+                 if(base[id].type === "c") {
+                     sub = id.substr(1,id.length);
+                     Levels["sub"]={"id":sub,"order":ord};
+                     ord++;
+                 }
+                 // sinon, c'est un epci, un SCoT ou autre
+                 // dans tous les cas on repart de la commune nouvelle
+                 id = "i"+com;
+             }
+             else com = id.substr(1,id.length);
+             Levels["com"]={"id":com,"order":ord};ord++;
+             if(base[id].e !== undefined) {
+                 Levels["epci"]={"id":base[id].e,"order":ord};
+                 ord++};
+             Levels["dep"]={"id":com.substr(0,2),"order":ord};ord++;
+             Levels["reg"]={"id":"Norm","order":ord};
+             return Levels;
+         }
         /* fonction de récupération du nom de territoire
         function searchIn(base,field,val) {
             for(var i in base) {
